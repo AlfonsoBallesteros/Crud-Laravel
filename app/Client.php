2e53d22cd31f89b4094c2a6d72c44b2b9d00c80c
Alfonso Ballesteros <ponchob1258@gmail.com>
2020-08-29 14:12:02 -0500

    feat: Encrypt

--- app/Client.php
+++ app/Client.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Encryptable;
 
 class Client extends Model
 {
+    use Encryptable;
     
+    protected $encryptable = [
+        'name', 'email',
+    ];
+
     protected $fillable = [
         'name',
         'email',
